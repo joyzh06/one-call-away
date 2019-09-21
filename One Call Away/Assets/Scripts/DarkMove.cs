47299f0a0eccb5337a50db839c946fcfc8ef2e75
Joy Zhang <jz442@cornell.edu>
2019-09-21 18:58:41 -0400

    started a level

---
+++ One Call Away/Assets/Scripts/DarkMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkMove : MonoBehaviour {
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = player.transform.position + new Vector3(-0.55f, 1.5f, 0);
+		
+	}
+}
